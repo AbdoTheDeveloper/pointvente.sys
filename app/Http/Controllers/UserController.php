2c6fce701e7c12c46aa8b25c0fa6f4ff2c6ac8c6
EL KASBAJI Abdelhadi <akasbaji2@gmail.com>
2023-11-27 18:13:15 +0100

    best sellers code added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ use App\Model\Categorie;
 use App\Model\Prod;
 use App\Model\Operation_prod;
 use App\Model\Fornisseur;
-
+use DB; 
 use App\Http\Controllers\Session;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -60,8 +60,8 @@ class UserController  extends Controller
         
 
          
-        $classes_arr = Prod::where('qte','<=','qte_alert')->get();
-        // dd() ; 
+        $classes_arr = DB::table('prods')->select('*')->where("qte", "<=" , "qte_alert")->get() ; 
+        dd($classes_arr) ; 
 
         $classes = Travailleur::all()->count();
 
